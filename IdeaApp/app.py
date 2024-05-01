9e5aa5e4448ef3e8dc4585459f58e0aae307045e
CodeWithKaleem <kaleemshaikh1173@gmail.com>
2024-05-01 17:45:47 +0530

    Post code done

--- IdeaApp/app.py
+++ IdeaApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask,request
 
 app = Flask(__name__)
 
@@ -25,7 +25,27 @@ Create a Restful endpoint for fetching all the ideas
 def get_all_idea():
     return ideas
 
+'''
+Create a Restful enpoint for creating an new idea
+'''
+@app.post("/IdeaApp/api/v1/ideas")
+def create_idea():
+    # Login to create idea
+    try :
+        # first read the request body
+        request_body = request.get_json()
 
+        # check if passed idea is not present already
+        if request_body["id"] and request_body["id"] in ideas:
+            return 'idea with the same id is already present',400
+        # Insert the passed idea into the ideas dictionary
+        ideas[request_body["id"]] = request_body
+        # Return the response saying idea saved successfully
+        return 'Idea created and saved successfully',201
+    except KeyError:
+        return 'id is missing',400
+    except:
+        return "Some internal server error",500
 
 if __name__ == '__main__':
     app.run(port=8080)
